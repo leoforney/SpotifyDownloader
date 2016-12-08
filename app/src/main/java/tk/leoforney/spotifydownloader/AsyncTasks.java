b8ed916496ad6b8b54c16d30f6dfaf6e6d683dd4
Leo Forney <forneyleo@gmail.com>
2016-12-07 23:24:28 -0600

    Correctly finds all youtube equivilant of each song. Also correctly lists all songs from user

--- app/src/main/java/tk/leoforney/spotifydownloader/AsyncTasks.java
+++ app/src/main/java/tk/leoforney/spotifydownloader/AsyncTasks.java
@@ -4,22 +4,18 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.util.Log;
 
-import com.github.axet.vget.VGet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import kaaes.spotify.webapi.android.models.Pager;
 import kaaes.spotify.webapi.android.models.PlaylistSimple;
 import kaaes.spotify.webapi.android.models.PlaylistTrack;
-import kaaes.spotify.webapi.android.models.Track;
 import kaaes.spotify.webapi.android.models.UserPrivate;
 
 import static tk.leoforney.spotifydownloader.MainActivity.spotify;
@@ -69,43 +65,24 @@ public class AsyncTasks {
             options = new HashMap<>();
             trackPager = spotify.getPlaylistTracks(user, id);
             int total = downloads[0].playlist.tracks.total;
-            int amountOfHundreds = Math.round(total/100);
-            Log.d(TAG, String.valueOf(total + ":" + amountOfHundreds));
+            int amountOfHundreds = Math.round(total / 100);
             List<PlaylistTrack> allTracksInPlaylist = new ArrayList<>();
 
-            if (total <= 100 && total > 0) {
-                trackPager = spotify.getPlaylistTracks(user, id);
-                allTracksInPlaylist.addAll(trackPager.items);
-            }
-            if (total <= 200 && total > 100) {
-                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id).items);
-                options.put("offset", 100);
+            allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id).items);
+            for (int i = 0; i < amountOfHundreds; i++) {
+                Log.d(TAG, "Code Ran!");
+                options.put("offset", 100*amountOfHundreds);
                 allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
             }
-            if (total <= 300 && total > 200) {
-                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
-                options.put("offset", 100);
-                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
-                options.put("offset", 200);
-                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
-            }
-            if (total <= 400 && total > 300) {
-                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
-                options.put("offset", 100);
-                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
-                options.put("offset", 200);
-                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
-                options.put("offset", 300);
-                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
-            }
+            Log.d(TAG, String.valueOf(total + " - " + allTracksInPlaylist.size()));
 
             List<LocalSong> allLocalSongs = new ArrayList<>();
             for (PlaylistTrack playlistTrack : allTracksInPlaylist) {
                 LocalSong song = new LocalSong();
+                song.track = playlistTrack;
                 song.searchItems = Lists.newArrayList(Iterables.limit(yc.search(playlistTrack.track.name + playlistTrack.track.artists.get(0).name), 5));
-                for (VideoItem items : song.searchItems) {
-                    Log.d(TAG, items.getTitle());
-                }
+                Log.d(TAG, song.track.track.name + ": " + song.searchItems.get(0).getTitle() + " - "
+                        + "https://youtube.com/watch?v=" + song.searchItems.get(0).getId());
                 song.track = playlistTrack;
                 allLocalSongs.add(song);
             }
@@ -115,13 +92,11 @@ public class AsyncTasks {
                 try {
                     File downloadFile = new File(Environment.getExternalStorageDirectory() + "/Music/" + downloads[0].playlist.name);
                     URL downloadURL = new URL("https://www.youtube.com/watch?v=" + song.searchItems.get(0).getId());
-                    Log.d(TAG, downloadURL.toString());
                     //new VGet(downloadURL, downloadFile).download();
                 } catch (Exception q) {
                     q.printStackTrace();
                 }
             }
-            Log.d(TAG, String.valueOf(total + ":" + allTracksInPlaylist.size()));
             return null;
         }
 
@@ -130,33 +105,6 @@ public class AsyncTasks {
             super.onProgressUpdate(values);
 
         }
-    }
-
-    private void downloadPlaylistFromPager(HashMap<String, Object> options, int degree, List<PlaylistTrack> allTracksInPlaylist, String user, String id) {
-        options.put("offset", 200);
-        Pager<PlaylistTrack> thirdClassPlaylistTracks = spotify.getPlaylistTracks(user, id, options);
-        allTracksInPlaylist.addAll(thirdClassPlaylistTracks.items);
-    }
-
-    private static int roundClassNumber(int number) {
-
-        String returnString = "";
-        int returnNumber;
-        int firstDigit = Integer.parseInt(Integer.toString(number).substring(0, 1));
-        int amtDigits = (int) (Math.log10(number) + 1);
-        String thresholdString = String.valueOf(firstDigit);
-        for (int i = 0; i < amtDigits - 1; i++) {
-            thresholdString = thresholdString + "0";
-        }
-        int threshold = Integer.valueOf(thresholdString);
-        if (number > threshold) {
-            returnString = String.valueOf(firstDigit + 1);
-            for (int i = 0; i < amtDigits - 1; i++) {
-                thresholdString = thresholdString + "0";
-            }
-        }
-        returnNumber = Integer.valueOf(returnString);
-        return returnNumber;
     }
 
 }
