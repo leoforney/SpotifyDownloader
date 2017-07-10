6d94d716643224b9031017b23c0fefd4cec7f13a
Leo Forney <admin@example.com>
2017-07-09 20:32:22 -0500

    Semi working build, needs some UI improvements but almost working AsyncTasks. About 80% reliability for playlists as some complications occur between finding songs on youtube and actually downloading them.

--- app/src/main/java/tk/leoforney/spotifydownloader/AsyncTasks.java
+++ app/src/main/java/tk/leoforney/spotifydownloader/AsyncTasks.java
@@ -5,20 +5,35 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.util.Log;
 
-import com.github.axet.vget.VGet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.io.Files;
+import com.google.gson.Gson;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import kaaes.spotify.webapi.android.models.Pager;
 import kaaes.spotify.webapi.android.models.PlaylistSimple;
 import kaaes.spotify.webapi.android.models.PlaylistTrack;
 import kaaes.spotify.webapi.android.models.UserPrivate;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import static tk.leoforney.spotifydownloader.MainActivity.rvAdapter;
 import static tk.leoforney.spotifydownloader.MainActivity.spotify;
@@ -60,33 +75,43 @@ public class AsyncTasks {
         HashMap<String, Object> options;
         Pager<PlaylistTrack> trackPager;
         int index;
+        Gson gson;
 
         @Override
         protected Void doInBackground(List<Object>... downloads) {
-            try {
-                PlaylistDownload downloadRequest = (PlaylistDownload) downloads[0].get(0);
-                Context context = (Context) downloads[0].get(1);
-                index = (Integer) downloads[0].get(2);
-                YoutubeConnector yc = new YoutubeConnector(context);
-                user = downloadRequest.playlist.owner.id;
-                id = downloadRequest.playlist.id;
-                options = new HashMap<>();
-                trackPager = spotify.getPlaylistTracks(user, id);
-                int total = downloadRequest.playlist.tracks.total;
-                int amountOfHundreds = Math.round(total / 100);
-                List<PlaylistTrack> allTracksInPlaylist = new ArrayList<>();
+            PlaylistDownload downloadRequest = (PlaylistDownload) downloads[0].get(0);
+            Context context = (Context) downloads[0].get(1);
+            index = (Integer) downloads[0].get(2);
+            YoutubeConnector yc = new YoutubeConnector(context);
+            gson = new Gson();
+            user = downloadRequest.playlist.owner.id;
+            id = downloadRequest.playlist.id;
+            options = new HashMap<>();
+            trackPager = spotify.getPlaylistTracks(user, id);
+            int total = downloadRequest.playlist.tracks.total;
+            int amountOfHundreds = Math.round(total / 100);
+            List<PlaylistTrack> allTracksInPlaylist = new ArrayList<>();
 
-                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id).items);
-                for (int i = 0; i < amountOfHundreds; i++) {
-                    options.put("offset", 100 * amountOfHundreds);
-                    allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
-                }
-                Log.d(TAG, String.valueOf(total + " - " + allTracksInPlaylist.size()));
+            allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id).items);
+            for (int i = 0; i < amountOfHundreds; i++) {
+                options.put("offset", 100 * amountOfHundreds);
+                allTracksInPlaylist.addAll(spotify.getPlaylistTracks(user, id, options).items);
+            }
+            Log.d(TAG, String.valueOf(total + " - " + allTracksInPlaylist.size()));
 
-                List<LocalSong> allLocalSongs = new ArrayList<>();
-                for (PlaylistTrack playlistTrack : allTracksInPlaylist) {
-                    LocalSong song = new LocalSong();
-                    song.track = playlistTrack;
+            List<LocalSong> allLocalSongs = new ArrayList<>();
+            for (PlaylistTrack playlistTrack : allTracksInPlaylist) {
+                LocalSong song = new LocalSong();
+                song.track = playlistTrack;
+                File downloadFile = new File(Environment.getExternalStorageDirectory() +
+                        File.separator +
+                        "Music" +
+                        File.separator +
+                        downloadRequest.playlist.name +
+                        File.separator +
+                        song.track.track.artists.get(0).name + " - " + song.track.track.name.replace("/", "") + ".mp3");
+                song.downloadLocaiton = downloadFile;
+                if (!song.downloadLocaiton.exists()) {
                     song.searchItems = Lists.newArrayList(Iterables.limit(yc.search(playlistTrack.track.name + "" + playlistTrack.track.artists.get(0).name), 5));
                     if (song.searchItems.size() == 0) {
                         song.searchItems = Lists.newArrayList(Iterables.limit(yc.search(playlistTrack.track.name + "" + playlistTrack.track.artists.get(0).name), 5));
@@ -96,24 +121,41 @@ public class AsyncTasks {
                     }
 
                     if (song.searchItems.size() != 0) {
-                        Log.d(TAG, song.track.track.name + ": " + song.searchItems.get(0).getTitle() + " - "
+                        Log.d(TAG, song.track.track.name + " - "
                                 + "https://youtube.com/watch?v=" + song.searchItems.get(0).getId());
-                        File downloadFile = new File(Environment.getExternalStorageDirectory() + "/Music/" + downloadRequest.playlist.name);
-                        URL downloadURL = new URL("https://www.youtube.com/watch?v=" + song.searchItems.get(0).getId());
+                        String videoDownloadURL = "https://www.youtube.com/watch?v=" + song.searchItems.get(0).getId();
+                        String totalDownloadURL = "http://www.youtubeinmp3.com/fetch/?video=" + videoDownloadURL;
 
-                        new VGet(downloadURL, downloadFile).download();
+                        OkHttpClient client = new OkHttpClient();
+
+                        try {
+                            Request request = new Request.Builder().url(totalDownloadURL).build();
+                            Response response = client.newCall(request).execute();
+
+                            byte[] bytes = response.body().bytes();
+
+                            String fileContent = new String(bytes);
+                            if (fileContent.contains("meta")) {
+                                Log.d(TAG, "Could not download song from youtube2mp3");
+                                song.correctlyDownloaded = false;
+                            } else {
+                                Log.d(TAG, "Properly downloaded song!");
+                                song.correctlyDownloaded = true;
+                                Files.write(bytes, downloadFile);
+                            }
+
+                            publishProgress(downloadRequest);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
-
-                    song.track = playlistTrack;
-                    allLocalSongs.add(song);
-
-                    float status = (allTracksInPlaylist.indexOf(playlistTrack) / (total-1));
-                    downloadRequest.status = status * 100;
-                    Log.d(TAG, String.valueOf(allTracksInPlaylist.indexOf(playlistTrack) + "/" + (total-1)));
-                    publishProgress(downloadRequest);
+                } else {
+                    Log.d(TAG, song.track.track.name + " is already downloaded!");
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+
+                song.track = playlistTrack;
+                allLocalSongs.add(song);
+
             }
 
             return null;
--- app/src/main/java/tk/leoforney/spotifydownloader/LocalSong.java
+++ app/src/main/java/tk/leoforney/spotifydownloader/LocalSong.java
@@ -1,6 +1,7 @@
 package tk.leoforney.spotifydownloader;
 
 import java.io.File;
+import java.net.URL;
 import java.util.List;
 
 import kaaes.spotify.webapi.android.models.PlaylistTrack;
@@ -10,7 +11,10 @@ import kaaes.spotify.webapi.android.models.PlaylistTrack;
  */
 
 public class LocalSong {
-    List<VideoItem> searchItems;
-    PlaylistTrack track;
-    File downloadLocaiton;
+    public List<VideoItem> searchItems;
+    public PlaylistTrack track;
+    public File downloadLocaiton;
+    public transient String fileContent;
+    public transient boolean correctlyDownloaded;
+    public transient String downloadURL;
 }
